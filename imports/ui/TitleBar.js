c60b7fc80f3d425d05a7099d2136bd940b79080e
Arnaud Panapadéatchy <arnaud.panapadeatchy@dsinstruments.fr>
2019-12-16 14:49:37 +0400

    Commit before styling

---
+++ imports/ui/TitleBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+export default class TitleBar extends React.Component {
+  renderSubtitle() {
+    if (this.props.subtitle) {
+      return <h2>{this.props.subtitle}</h2>
+    } else {
+      return;
+    }
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.props.title}</h1>
+        {this.renderSubtitle()}
+      </div>
+    );
+  }
+};
+
+TitleBar.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string
+};
